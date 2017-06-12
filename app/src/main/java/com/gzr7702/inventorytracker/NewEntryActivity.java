056bf250daec2cef42cee256ed869071adac7d53
gzr7702 <rgraziano.gzr7702@gmail.com>
2017-06-11 20:54:57 -0400

    feat: Initial commit

---
+++ app/src/main/java/com/gzr7702/inventorytracker/NewEntryActivity.java
@@ -0,0 +1,18 @@
+package com.gzr7702.inventorytracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+
+/*
+    This activity is used to add a new inventory item
+ */
+
+public class NewEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_entry);
+    }
+}
